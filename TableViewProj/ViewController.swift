342dbd6fc4476d9d02fedb5d9fed3f89382196c4
chaewon-moana <dreamytaurus508@gmail.com>
2024-01-07 12:42:18 +0900

    Initial Commit

---
+++ TableViewProj/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewProj
+//
+//  Created by cho on 1/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
